7979cf20d7b2ef06c5a12ec3fee32e1b4f30d026
wushilin <wusl@126.com>
2022-05-07 22:00:00 +0800

    修改了Person.java

--- src/com/wsl/testGit/Person.java
+++ src/com/wsl/testGit/Person.java
@@ -1,10 +1,16 @@
 package com.wsl.testGit;
 
+import java.util.Deque;
+
 /**
  * @author:
  */
 public class Person {
     public static void main(String[] args) {
         System.out.println("hello world!");
+    }
+    public int addNum(int a, int b){
+        int ans = a + b;
+        return ans;
     }
 }
